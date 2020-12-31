40134b82d2367e16c5b154e14b7ad3b383132892
Zheng Ma <mazhengcn@outlook.com>
2020-12-31 08:04:43 +0000

    Finisha 1d ap-rbm test.

--- examples/linear_transport/rbm_1d.py
+++ examples/linear_transport/rbm_1d.py
@@ -6,14 +6,6 @@ from examples.utils import Progbar
 from kipack import collision, pykinetic
 from kipack.pykinetic.boltzmann.solver import BoltzmannSolver1D
 
-
-class DiffusiveRegimeSolver1D(BoltzmannSolver1D):
-    def dq(self, state):
-        deltaq = self.dq_hyperbolic(state) / self.kn
-        deltaq += self.dq_collision(state) / self.kn
-        return deltaq
-
-
 rkcoeff = {
     "RK3": {
         "a": np.array([[0.0, 0.0, 0.0], [0.5, 0.0, 0], [-1.0, 2.0, 0.0]]),
@@ -35,22 +27,6 @@ rkcoeff = {
 }
 
 
-def maxwellian(v, rho, u, T):
-    vdim, v_u = None, None
-    if isinstance(rho, np.ndarray):
-        v = np.asarray(v)
-        u = np.asarray(u)
-        xdim = rho.ndim
-        vdim = v.shape[0]
-        v_dim = np.index_exp[:] + xdim * (np.newaxis,)
-        v = v[v_dim]
-        q_dim = (...,) + vdim * (np.newaxis,)
-        rho, u, T = rho[q_dim], u[q_dim], T[q_dim]
-        v_u = np.sum((v - u) ** 2, axis=0)
-
-    return rho / (2 * math.pi * T) ** (vdim / 2) * np.exp(-(v_u ** 2) / 2 / T)
-
-
 def maxwellian_vec_init(vmesh, u, T, rho):
     v = vmesh.center
     return (
@@ -60,62 +36,10 @@ def maxwellian_vec_init(vmesh, u, T, rho):
     )
 
 
-def bkw_fn(vmesh, t):
-    vsq = vmesh.vsquare
-
-    K = 1 - 0.5 * np.exp(-t / 8)
-    return (
-        1
-        / (2 * math.pi * K ** 2)
-        * np.exp(-0.5 * vsq / K)
-        * (2 * K - 1 + 0.5 * vsq * (1 - K) / K)
-    )
-
-
-def flat(vmesh, T0):
-    vx, vy = vmesh.centers
-    w = np.sqrt(3 * T0)
-    return 1 / 4 / w ** 2 * (vx <= w) * (vx >= -w) * (vy <= w) * (vy >= -w)
-
-
-def maxwellian_init(vmesh, K):
-    return 1 / (2 * math.pi * K) * np.exp(-0.5 * vmesh.vsq / K)
-
-
-def anisotropic_f(v):
-    return (
-        0.8
-        * math.pi ** (-1.5)
-        * (
-            np.exp(
-                -(16 ** (1 / 3))
-                * (
-                    (v - 2)[:, None, None] ** 2
-                    + (v - 2)[:, None] ** 2
-                    + (v - 2) ** 2
-                )
-            )
-            + np.exp(
-                -(v + 0.5)[:, None, None] ** 2
-                - (v + 0.5)[:, None] ** 2
-                - (v + 0.5) ** 2
-            )
-        )
-    )
-
-
-def qinit(state, vmesh, init_func=None):
+def qinit(state, vmesh, init_func):
     x = state.grid.x.centers
     rho = 1.0 + 0.5 * np.cos(4 * math.pi * x)
-    if init_func:
-        state.q[0, ...] = init_func(vmesh, 0.0, 1.0, rho)
-    else:
-        state.q[0, ...] = 0.0
-
-
-def sigma(x):
-    # return (x < 0.5) * 2.0 + (x >= 0.5) * 1.0
-    return np.ones(x.shape) * 1.0
+    state.q[0, ...] = init_func(vmesh, 0.0, 1.0, rho)
 
 
 def compute_rho(state, vmesh):
@@ -123,14 +47,52 @@ def compute_rho(state, vmesh):
     return 0.5 * np.sum(state.q[0, ...] * vmesh.weights, axis=vaxis)
 
 
+class DiffusiveRegimeSolver1D(BoltzmannSolver1D):
+    def __init__(
+        self,
+        riemann_solver,
+        collision_operator,
+        kn,
+        sigma_s,
+        sigma_a,
+        Q,
+        **kwargs
+    ):
+        self.sigma_s, self.sigma_a, self.Q = map(
+            self._convert_params, [sigma_s, sigma_a, Q]
+        )
+
+        super().__init__(
+            riemann_solver=riemann_solver,
+            collision_operator=collision_operator,
+            kn=kn,
+            **kwargs,
+        )
+
+    def dq(self, state):
+        deltaq = self.dq_hyperbolic(state) / self.kn
+        deltaq += self.dq_collision(state) / self.kn * self.sigma_s
+        deltaq -= self.sigma_a * state.q * self.dt
+        deltaq += self.Q * self.dt
+        return deltaq
+
+
 def run(
     kn=1.0,
+    sigma_s=lambda x: 1.0,
+    sigma_a=lambda x: 0.0,
+    Q=lambda x: 0.0,
+    xmin=0.0,
+    xmax=1.0,
+    nx=40,
     dt=0.01,
     nt=1000,
     coll="linear",
     scheme="Euler",
     BC="periodic",
-    init_func=None,
+    f_l=lambda v: 1.0,
+    f_r=lambda v: 0.0,
+    init_func=lambda vmesh, u, T, rho: 0.0,
 ):
     # Load config
     config = collision.utils.CollisionConfig.from_json(
@@ -151,13 +113,18 @@ def run(
         )
 
     # x domian
-    x = pykinetic.Dimension(0.0, 1.0, 40, name="x")
+    x = pykinetic.Dimension(xmin, xmax, nx, name="x")
     domain = pykinetic.Domain([x])
 
     # Riemann solver
     rp = pykinetic.riemann.advection_1D
     solver = DiffusiveRegimeSolver1D(
-        rp, coll_op, kn=kn / sigma(x.centers)[:, None]
+        rp,
+        coll_op,
+        kn=kn(x.centers),
+        sigma_s=sigma_s(x.centers),
+        sigma_a=sigma_a(x.centers),
+        Q=Q(x.centers),
     )
     solver.order = 1
     # solver.lim_type = 2
@@ -176,12 +143,12 @@ def run(
     def dirichlet_lower_BC(state, dim, t, qbc, auxbc, num_ghost):
         v = state.problem_data["v"][0]
         for i in range(num_ghost):
-            qbc[0, i, v > 0] = 1.0 + np.cos(v)[v > 0]
+            qbc[0, i, v > 0] = f_l(v[v > 0])
 
     def dirichlet_upper_BC(state, dim, t, qbc, auxbc, num_ghost):
         v = state.problem_data["v"][0]
         for i in range(num_ghost):
-            qbc[0, -i - 1, v < 0] = 0.0 + 2 * np.cos(v)[v < 0]
+            qbc[0, -i - 1, v < 0] = f_r(v[v < 0])
 
     if BC == "periodic":
         solver.bc_lower[0] = pykinetic.BC.periodic
@@ -200,16 +167,22 @@ def run(
     sol = pykinetic.Solution(state, domain)
 
     output_dict = {}
-    sol_frames = []
-    macro_frames = []
+    sol_frames, macro_frames, ts = (
+        [copy.deepcopy(sol)],
+        [compute_rho(sol.state, vmesh)],
+        [0.0],
+    )
     pbar = Progbar(nt)
     for t in range(nt):
         solver.evolve_to_time(sol)
-        macro_frames.append(compute_rho(sol.state, vmesh))
         sol_frames.append(copy.deepcopy(sol))
+        macro_frames.append(compute_rho(sol.state, vmesh))
+        ts.append(0.0 + (t + 1) * dt)
         pbar.update(t + 1, finalize=False)
     pbar.update(nt, finalize=True)
 
     output_dict["macro_frames"] = macro_frames
+    output_dict["x"] = x.centers
+    output_dict["t"] = ts
 
     return output_dict
--- examples/linear_transport/rbm_1d_ap.py
+++ examples/linear_transport/rbm_1d_ap.py
@@ -23,19 +23,12 @@ def qinit(state, vmesh, kn, init_func=None):
     x = state.grid.x.centers
     rho = 1.0 + 2.0 * np.cos(4 * math.pi * x)
     v = vmesh.center
-    if init_func:
-        pos_v = init_func(v, 0.0, 1.0, rho)
-        # print(pos_v)
-        min_v = init_func(-v, 0.0, 1.0, rho)
-        state.q[0, :] = 0.5 * (pos_v + min_v)
-        state.q[1, :] = 0.5 / kn * (pos_v - min_v)
-    else:
-        state.q[:] = 0.0
 
-
-def sigma(x):
-    # return (x < 0.5) * 2.0 + (x >= 0.5) * 1.0
-    return np.ones(x.shape) * 1.0
+    pos_v = init_func(v, 0.0, 1.0, rho)
+    # print(pos_v)
+    min_v = init_func(-v, 0.0, 1.0, rho)
+    state.q[0, :] = 0.5 * (pos_v + min_v)
+    state.q[1, :] = 0.5 / kn * (pos_v - min_v)
 
 
 def compute_rho(state, vmesh):
@@ -44,6 +37,27 @@ def compute_rho(state, vmesh):
 
 
 class APNeutronTransportSolver1D(BoltzmannSolver1D):
+    def __init__(
+        self,
+        riemann_solver,
+        collision_operator,
+        kn,
+        sigma_s,
+        sigma_a,
+        Q,
+        **kwargs
+    ):
+        self.sigma_s, self.sigma_a, self.Q = map(
+            self._convert_params, [sigma_s, sigma_a, Q]
+        )
+
+        super().__init__(
+            riemann_solver=riemann_solver,
+            collision_operator=collision_operator,
+            kn=kn,
+            **kwargs,
+        )
+
     def step_collision(self, state):
         dt_kn2 = self.dt / self.kn ** 2
 
@@ -52,7 +66,9 @@ class APNeutronTransportSolver1D(BoltzmannSolver1D):
             self.coll[0](state.q[0, :], heat_bath=self.tau, device=self.device)
             + state.q[0, :]
         ) * 2
-        state.q[0, :] = (dt_kn2 * rho + state.q[0, :]) / (1.0 + dt_kn2)
+        state.q[0, :] = (dt_kn2 * self.sigma_s * rho + state.q[0, :]) / (
+            1.0 + dt_kn2 * self.sigma_s
+        )
 
         # Compute r_x
         num_ghost = self.num_ghost
@@ -70,22 +86,32 @@ class APNeutronTransportSolver1D(BoltzmannSolver1D):
         state.q[1, :] = (
             state.q[1, :] - dt_kn2 * (1.0 - self.kn ** 2 * phi) * v * dr_dx
         )
-        state.q[1, :] /= 1.0 + dt_kn2
+        state.q[1, :] /= 1.0 + dt_kn2 * self.sigma_s
 
     def dq(self, state):
         self.step_collision(state)
         deltaq = self.dq_hyperbolic(state)
+        deltaq -= self.sigma_a * state.q * self.dt
+        deltaq[0, :] += self.Q * self.dt
         return deltaq
 
 
 def run(
-    kn=1.0,
+    kn=lambda x: 1.0,
+    sigma_s=lambda x: 1.0,
+    sigma_a=lambda x: 0.0,
+    Q=lambda x: 0.0,
+    xmin=0.0,
+    xmax=1.0,
+    nx=40,
     dt=0.01,
     nt=1000,
     coll="linear",
     scheme="Euler",
     BC="periodic",
-    init_func=None,
+    f_l=lambda v: 1.0,
+    f_r=lambda v: 0.0,
+    init_func=lambda vmesh, u, T, rho: 0.0,
 ):
     # Load config
     config = collision.utils.CollisionConfig.from_json(
@@ -94,6 +120,7 @@ def run(
 
     # Collision
     vmesh = collision.CartesianMesh(config)
+    # print(vmesh.centers[0], vmesh.weights)
     if coll == "linear":
         coll_op = collision.LinearCollision(config, vmesh)
     elif coll == "rbm":
@@ -106,12 +133,20 @@ def run(
         )
 
     # x domian
-    x = pykinetic.Dimension(0.0, 1.0, 40, name="x")
+    x = pykinetic.Dimension(xmin, xmax, nx, name="x")
+    # print(x.centers_with_ghost(2))
     domain = pykinetic.Domain([x])
 
     # Riemann solver
     rp = pykinetic.riemann.parity_1D
-    solver = APNeutronTransportSolver1D(rp, [coll_op], kn=kn)
+    solver = APNeutronTransportSolver1D(
+        rp,
+        [coll_op],
+        kn=kn(x.centers),
+        sigma_s=sigma_s(x.centers),
+        sigma_a=sigma_a(x.centers),
+        Q=Q(x.centers),
+    )
     # print(solver.kn)
     solver.order = 1
     # solver.lim_type = -1
@@ -119,33 +154,43 @@ def run(
     solver.dt = dt
     print("dt is {}".format(solver.dt))
 
-    fL = 1.0
-    fR = 0.0
+    sigma = sigma_s(x.centers) + kn(x.centers) ** 2 * sigma_a(x.centers)
+    sigma_l, sigma_r = None, None
+    if isinstance(sigma, np.ndarray):
+        sigma_l, sigma_r = sigma[0], sigma[-1]
+    else:
+        sigma_l = sigma_r = sigma
+    kn_l, kn_r = None, None
+    if isinstance(solver.kn, np.ndarray):
+        kn_l, kn_r = solver.kn[0], solver.kn[-1]
+    else:
+        kn_l = kn_r = solver.kn
 
     # Boundary conditions
     def dirichlet_lower_BC(state, dim, t, qbc, auxbc, num_ghost):
-        v = state.problem_data["v"][0]
-        kn_dx = kn / state.grid.delta[0]
+        v = state.problem_data["v"][0] / sigma_l
+        kn_dx = kn_l / state.grid.delta[0]
         for i in range(num_ghost):
-            qbc[0, i, :] = (fL - (0.5 - kn_dx * v) * qbc[0, num_ghost, :]) / (
-                0.5 + kn_dx * v
-            )
+            qbc[0, i, :] = (
+                f_l(v) - (0.5 - kn_dx * v) * qbc[0, num_ghost, :]
+            ) / (0.5 + kn_dx * v)
         for i in range(num_ghost):
             qbc[1, i, :] = (
-                2 * fL - (qbc[0, num_ghost - 1, :] + qbc[0, num_ghost, :])
-            ) / kn - qbc[1, num_ghost, :]
+                2 * f_l(v) - (qbc[0, num_ghost - 1, :] + qbc[0, num_ghost, :])
+            ) / kn_l - qbc[1, num_ghost, :]
 
     def dirichlet_upper_BC(state, dim, t, qbc, auxbc, num_ghost):
-        v = state.problem_data["v"][0]
-        kn_dx = kn / state.grid.delta[0]
+        v = state.problem_data["v"][0] / sigma_r
+        kn_dx = kn_r / state.grid.delta[0]
         for i in range(num_ghost):
             qbc[0, -i - 1, :] = (
-                fR - (0.5 - kn_dx * v) * qbc[0, -num_ghost - 1, :]
+                f_r(v) - (0.5 - kn_dx * v) * qbc[0, -num_ghost - 1, :]
             ) / (0.5 + kn_dx * v)
         for i in range(num_ghost):
             qbc[1, -i - 1, :] = (
-                (qbc[0, -num_ghost, :] + qbc[0, -num_ghost - 1, :]) - 2 * fR
-            ) / kn - qbc[1, -num_ghost - 1, :]
+                (qbc[0, -num_ghost, :] + qbc[0, -num_ghost - 1, :])
+                - 2 * f_r(v)
+            ) / kn_r - qbc[1, -num_ghost - 1, :]
 
     if BC == "periodic":
         solver.bc_lower[0] = pykinetic.BC.periodic
@@ -160,24 +205,42 @@ def run(
 
     state = pykinetic.State(domain, vmesh, 2)
     state.problem_data["v"] = vmesh.centers
-    state.problem_data["phi"] = phi(kn)
-    state.problem_data["sqrt_phi"] = np.sqrt(phi(kn))
+    state.problem_data["phi"] = phi(solver.kn)
+    state.problem_data["sqrt_phi"] = np.sqrt(phi(solver.kn))
 
-    qinit(state, vmesh, kn, init_func)
+    qinit(state, vmesh, solver.kn, init_func)
     sol = pykinetic.Solution(state, domain)
 
     output_dict = {}
-    sol_frames = []
-    macro_frames = []
+    sol_frames, macro_frames, ts = (
+        [copy.deepcopy(sol)],
+        [compute_rho(sol.state, vmesh)],
+        [0.0],
+    )
     pbar = Progbar(nt)
     for t in range(nt):
-        macro_frames.append(compute_rho(sol.state, vmesh))
         solver.evolve_to_time(sol)
         sol_frames.append(copy.deepcopy(sol))
+        macro_frames.append(compute_rho(sol.state, vmesh))
+        # Test
+        # qbc = solver.qbc
+        # print(
+        #     np.max(
+        #         np.abs(
+        #             5.0 * np.sin(vmesh.centers[0])
+        #             - (
+        #                 0.5 * (qbc[0, 1] + qbc[0, 2])
+        #                 + kn * 0.5 * (qbc[1, 1] + qbc[1, 2])
+        #             )
+        #         )
+        #     )
+        # )
+        ts.append(0.0 + (t + 1) * dt)
         pbar.update(t + 1, finalize=False)
     pbar.update(nt, finalize=True)
-    macro_frames.append(compute_rho(sol.state, vmesh))
 
     output_dict["macro_frames"] = macro_frames
+    output_dict["x"] = x.centers
+    output_dict["t"] = ts
 
     return output_dict
