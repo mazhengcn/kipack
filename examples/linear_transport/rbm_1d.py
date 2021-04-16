534fe7d0cca0d87e6fc0139bc096525630d344fa
Zheng Ma <mazhengcn@outlook.com>
2021-04-16 19:47:06 +0800

    Update environment files.

--- examples/linear_transport/rbm_1d.py
+++ examples/linear_transport/rbm_1d.py
@@ -29,11 +29,7 @@ rkcoeff = {
 
 def maxwellian_vec_init(vmesh, u, T, rho):
     v = vmesh.center
-    return (
-        rho[:, None]
-        / np.sqrt(2 * math.pi * T)
-        * np.exp(-((v - u) ** 2) / (2 * T))
-    )
+    return rho[:, None] / np.sqrt(2 * math.pi * T) * np.exp(-((v - u) ** 2) / (2 * T))
 
 
 def qinit(state, vmesh, init_func):
@@ -49,14 +45,7 @@ def compute_rho(state, vmesh):
 
 class DiffusiveRegimeSolver1D(BoltzmannSolver1D):
     def __init__(
-        self,
-        riemann_solver,
-        collision_operator,
-        kn,
-        sigma_s,
-        sigma_a,
-        Q,
-        **kwargs
+        self, riemann_solver, collision_operator, kn, sigma_s, sigma_a, Q, **kwargs
     ):
         self.sigma_s, self.sigma_a, self.Q = map(
             self._convert_params, [sigma_s, sigma_a, Q]
